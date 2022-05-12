85cc84268b92eecd81bcc6e84932d169cd700040
Mert98 <102909932+onurdemirors32@users.noreply.github.com>
2022-05-12 10:28:17 +0300

    Add files via upload

---
+++ lib/controllers/auctionController.dart
@@ -0,0 +1,56 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:myapp/models/auction.dart';
+
+class AuctionController{
+
+    List <Auction> getAuctions(){
+        //devam edecek
+    }
+
+    Auction getAuction(AuctionID){
+        //devam edecek
+    }
+
+    Auction createAuction(){
+        //devam edecek
+    }
+
+    void deleteAuction(AuctionID,Type){
+        //devam edecek
+    }
+
+
+    List <User> getUsersInAuction(){
+        //devam edecek
+    }
+
+    User getUserInAuction(UserID){
+        //devam edecek
+    }
+
+    void startVideoStream(){
+        //devam edecek
+    }
+
+    void closeAuction(){
+        //devam edecek
+    }
+    auctionController{(
+
+        getAuctions(),
+        getAuction(),
+        createAuction(),
+        deleteAuction(AuctionID,Type),
+        getUsersInAuction()
+        getUserInAuction(UserID)
+        startVideoStream(),
+        closeAuction(),
+
+    );}
+
+
+
+
+
+
+}
---
+++ lib/controllers/auctionTableController.dart
@@ -0,0 +1,45 @@
+import 'package:myapp/models/auctionTable.dart';
+
+
+class AuctionTableController{
+
+
+
+  void addProductToTable(){
+
+
+
+  }
+
+  void deleteProductFromTable(){
+
+
+
+  }
+
+  void getTables(){
+
+
+
+  }
+
+  void getTable(){
+
+
+
+  }
+
+  void addAuctionTable(){
+
+
+
+  }
+
+  void deleteAuctionTable(){
+
+
+
+  }
+
+
+}
