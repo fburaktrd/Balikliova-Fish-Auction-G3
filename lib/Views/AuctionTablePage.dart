780c9440086060431cf2f40e59adff897a092058
Faik Burak Türedi <fburakturedi@hotmail.com>
2022-05-18 00:37:59 +0300

    added auction table getIt instance

--- lib/Views/AuctionTablePage.dart
+++ lib/Views/AuctionTablePage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/Views/navBar.dart';
+import 'package:myapp/controllers/AuctionTableGetItController.dart';
 import "package:myapp/controllers/auctionTableController.dart";
+import 'package:myapp/locator.dart';
 
 class AuctionTableScreen extends StatefulWidget {
   const AuctionTableScreen({Key? key}) : super(key: key);
@@ -13,41 +15,50 @@ class _AuctionTableScreenState extends State<AuctionTableScreen> {
   late AuctionTableController controller;
   Map<String, Map<String, dynamic>> auct_form = {};
   List<List<List<dynamic>>> tablesList = [];
-  List<List<dynamic>> oneTable = [];
+  List<List<dynamic>> oneTable = getIt<AuctionTableGetItController>().getTable;
 
   final _productNameController = TextEditingController();
   final _quantityController = TextEditingController();
   final _basePriceController = TextEditingController();
   int rowNo = 0;
-  bool show_auct_table = false;
-  Map<String, Map<String,Map<String,dynamic>>> tables = {"tables": {}};
-
+  bool show_auct_table=false;
+  Map<String, Map<String, Map<String, dynamic>>> tables = {"tables": {}};
+  @override
+  void initState() {
+    show_auct_table = oneTable.length == 0 ? false : true;
+    setState(() {
+      
+    });
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
             title: const Text("Create Auction Table"), centerTitle: true),
         drawer: navBar(),
         body: SafeArea(
-          child: 
-            Padding(padding: EdgeInsets.symmetric(horizontal: 10.0),
-              child: Column(
-          children: [    
-            FloatingActionButton(
-              child: const Icon(Icons.add),
-              onPressed: () {
-                setState(() {
-                  addAuctionTableAlert(context);
-                });
-              },
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: Column(
+              children: [
+                FloatingActionButton(
+                  child: const Icon(Icons.add),
+                  onPressed: () {
+                    setState(() {
+                      addAuctionTableAlert(context);
+                    });
+                  },
+                ),
+                show_auct_table == true
+                    ? showAuctionTable(oneTable)
+                    : Text("You have no auction table...")
+              ],
             ),
-            show_auct_table == true ? showAuctionTable(oneTable) : Text("You have no auction table...")
-          ],
+          ),
         ),
-        
-      ),
-    ),
       ),
     );
   }
@@ -110,27 +121,29 @@ class _AuctionTableScreenState extends State<AuctionTableScreen> {
                         int.parse(_basePriceController.text);
                     auct_form[rowNo.toString()]!["soldPrice"] = 0;
 
-                    oneTable.add([rowNo.toString(), 
-                                  _productNameController.text,
-                                  int.parse(_quantityController.text), 
-                                  int.parse(_basePriceController.text),
-                                  0]);
+                    oneTable.add([
+                      rowNo.toString(),
+                      _productNameController.text,
+                      int.parse(_quantityController.text),
+                      int.parse(_basePriceController.text),
+                      0
+                    ]);
+                    getIt<AuctionTableGetItController>().fetchTable(oneTable);
+                    rowNo++;
                     tablesList.add(oneTable);
-                    
-                    
-                  setState(() {
-                    show_auct_table = true;
-                    _productNameController.clear();
-                    _basePriceController.clear();
-                    _quantityController.clear();
-                  });
 
-                  for (var key in auct_form.keys) {
-                    print(auct_form);
+                    setState(() {
+                      show_auct_table = true;
+                      _productNameController.clear();
+                      _basePriceController.clear();
+                      _quantityController.clear();
+                    });
+
+                    for (var key in auct_form.keys) {
+                      print(auct_form);
+                    }
+                    Navigator.of(context).pop();
                   }
-                  Navigator.of(context).pop();
-                      }
-                      
                 },
               ),
             ],
@@ -153,30 +166,31 @@ class _AuctionTableScreenState extends State<AuctionTableScreen> {
   }
 
   List<DataRow> getRows(List<List<dynamic>> table) =>
-  table.map((row) => 
-  DataRow(cells: getCells(row))).toList();
+      table.map((row) => DataRow(cells: getCells(row))).toList();
 
   List<DataCell> getCells(List<dynamic> row) =>
-    row.map((cell) => 
-    DataCell(Text('${cell}'))).toList();
+      row.map((cell) => DataCell(Text('${cell}'))).toList();
 
   List<DataColumn> getColumns(List<String> columns) =>
-    columns.map((column) => 
-    DataColumn(label: Text(column))
-    ).toList();
+      columns.map((column) => DataColumn(label: Text(column))).toList();
 
   Row showAuctionTable(List<List<dynamic>> oneTable) {
-    final columns = ["No", "Product Name", "Quantity", "Base Price", "Sold Price"];
+    final columns = [
+      "No",
+      "Product Name",
+      "Quantity",
+      "Base Price",
+      "Sold Price"
+    ];
 
-    Row card = Row(
-      children: [
-        Expanded(
-              child: DataTable(
-              columns: getColumns(columns),
-              rows: getRows(oneTable),),),]);
+    Row card = Row(children: [
+      Expanded(
+        child: DataTable(
+          columns: getColumns(columns),
+          rows: getRows(oneTable),
+        ),
+      ),
+    ]);
     return card;
   }
-
-
 }
-
---
+++ lib/controllers/AuctionTableGetItController.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:myapp/controllers/auctionTableController.dart';
+
+class AuctionTableGetItController extends ChangeNotifier {
+  List<List<dynamic>> _table = [];
+  AuctionTableGetItController();
+
+  fetchTable(dynamic table){
+    _table = table;
+    notifyListeners();
+  }
+   dynamic get getTable => _table;
+}
