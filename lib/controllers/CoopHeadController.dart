212be7a616f0b6cca412d2921a5988fc27dd49f5
Mert98 <102909932+Mert9898@users.noreply.github.com>
2022-05-27 14:02:30 +0300

    Hataları hallettim

--- lib/controllers/CoopHeadController.dart
+++ lib/controllers/CoopHeadController.dart
@@ -1,19 +1,20 @@
 import 'package:myapp/models/coopHead.dart';
-import 'package:myapp/views/AuctionTablePageForCoopHead.dart';
+import '../views/AuctionTablePageForCoopHead.dart';
+import 'UserController.dart';
 
 class CoopHeadController extends UserController {
-  
 
-    AuctionTablePageForCoopHead coopheadAuctionPage viewAuctionTables(){
-        //Burada auctionTablePageForCoophead i returnlemesi lazım ya da onun bir instance ını
-
+    viewAuctionTables() {
+        // TODO: implement changeProductFlow
+        throw UnimplementedError();
     }
 
-    void changeProductFlow(){
-        
-        //Eğer şu anki auciton bitmişse o zaman coophead database e bağlanıp yeni product i çekip auction create auctionTable ı çalıştırır
-
+    changeProductFlow() {
+      // TODO: implement changeProductFlow
+      throw UnimplementedError();
     }
+
+
 
  
 
