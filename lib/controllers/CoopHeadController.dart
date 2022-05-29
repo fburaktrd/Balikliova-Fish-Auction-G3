6df563e4219099b490296777821d9000b3245621
Mert98 <102909932+Mert9898@users.noreply.github.com>
2022-05-29 17:15:01 +0300

    Create CoopHeadController.dart

---
+++ lib/controllers/CoopHeadController.dart
@@ -0,0 +1,21 @@
+import 'package:myapp/models/coopHead.dart';
+import '../views/AuctionTablePageForCoopHead.dart';
+import 'UserController.dart';
+
+class CoopHeadController extends UserController {
+
+    viewAuctionTables() {
+        // TODO: implement changeProductFlow
+        throw UnimplementedError();
+    }
+
+    changeProductFlow() {
+      // TODO: implement changeProductFlow
+      throw UnimplementedError();
+    }
+
+
+
+ 
+
+}
