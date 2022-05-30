85bab6ca39717ed7585b2cc595c728a3ee1843e0
AsliCY <aslicandanyildirim@gmail.com>
2022-05-30 18:04:50 +0300

    update row bitti
    routing ayarlanacak

--- lib/Views/updateAuctionTable.dart
+++ lib/Views/updateAuctionTable.dart
@@ -11,13 +11,18 @@ class ViewAuctionTableCoopHead extends StatefulWidget {
       _ViewAuctionTableCoopHeadState();
 }
 class _ViewAuctionTableCoopHeadState extends State<ViewAuctionTableCoopHead> {
+
+  var routes = {'/viewCoopHead': (context) => ViewAuctionTableCoopHead()};
+  
+
   List<AuctionTable> auctionTables = [];
    List<List<dynamic>> table = [];
   List<List<List<dynamic>>> tables =    [
      [
        ["1", "Levrek", 20, 10, 10],
        ["2", "Hamsi", 12, 23, 43],
-       ["3", "Sardalya", 45, 3, 24]
+       ["3", "Sardalya", 45, 3, 24],
+
      ],
      [
        ["1", "Çipura", 20, 10, 10],
@@ -153,7 +158,7 @@ class _ViewAuctionTableCoopHeadState extends State<ViewAuctionTableCoopHead> {
               child: ElevatedButton(
                 child: Text("Update product"),
                 onPressed: () {
-                  updateRowAlert(context,table);
+                  updateRowAlert(context,tables[tableNum],tableNum);
                 },),
             ),
             Align(
@@ -270,16 +275,143 @@ class _ViewAuctionTableCoopHeadState extends State<ViewAuctionTableCoopHead> {
     });
   } 
 
-  void updateRowAlert(BuildContext context, List<List> table) {
-    
+  void updateRowAlert(BuildContext context, List<List<dynamic>> table, int tableNum) {
+    List<ElevatedButton> buttons = [];
+    for (var rowNum = 0; rowNum < table.length; rowNum++) {
+        ElevatedButton button = ElevatedButton(
+          child: Text("Update row ${rowNum+1}"),
+          onPressed: (){
+            updateAuctionTableAlert(context,tables[tableNum],tableNum,rowNum);
+                      },
+        );
+      buttons.add(button);
+
     showDialog(context: context, builder: (BuildContext context){
-      return AlertDialog();
-      
+      return AlertDialog(
+        title: Text("Choose row"),
+        content: Container(
+          width: double.minPositive,
+          child: ListView.builder(itemCount: buttons.length,
+          shrinkWrap: true,
+              itemBuilder: (context,index){
+                return buttons[index];
+              }
+            ),
+          ), 
+        actions: <Widget>[
+          ElevatedButton.icon(
+          icon: const Icon(Icons.cancel),
+          label: const Text("Cancel"),
+          onPressed: () {
+            Navigator.of(context).pop();
+            },
+          ),
+        ],                       
+        );
+                                
+    }).then((_) {
+      setState(() {});
     });
-    
+    }
   }
 
- 
-  }
+  void updateAuctionTableAlert(BuildContext context, List<List<dynamic>> table,int tableNum ,int rowNum) {
+        showDialog(context: context, builder: (BuildContext context){
+      return AlertDialog(
+        title: Text("Update product"),
+        content: Form(
+          key: _key,
+          child: Column(
+            children: [
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                controller: _productNameController,
+                decoration: const InputDecoration(labelText: 'Product Name'),
+                validator: (value) { 
+                  if (value!.isEmpty || _isNumeric(value)) {
+                    return 'Please enter a product name with letters';}
+                  else{
+                    return null;}
+                },
+                  onChanged: (value) => setState(() => productName = value)
+              ),
 
- 
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                controller: _quantityController,
+                decoration: const InputDecoration(labelText: 'Quantity'),
+                validator: (value) {
+                  
+                  if (value!.isEmpty || !(_isNumeric(value))) {
+                    return 'Please enter a quantity with numbers';}
+                  else{
+                    return null;}
+                  
+                },
+                onChanged: (value) => setState(() => quantity = value)
+                  
+                ),
+
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                controller: _basePriceController,
+                decoration: const InputDecoration(labelText: 'Base Price'),
+                validator: (value) {
+                  
+                  if (value!.isEmpty || !(_isNumeric(value))) {
+                    return 'Please enter a base price with numbers';}
+                  else{
+                    return null;}
+                },       
+                onChanged: (value) => setState(() => basePrice = value),
+              ),            
+            ],
+          ),
+          ),
+      
+       actions: <Widget>[
+         ElevatedButton.icon(
+          icon: const Icon(Icons.cancel),
+          label: const Text("Cancel"),
+          onPressed: () {
+            Navigator.of(context).pop(
+            MaterialPageRoute(
+              settings: RouteSettings(name: "/ViewAuctionTableCoopHead"),
+              builder: (context) => ViewAuctionTableCoopHead(),
+            ),
+          );
+            },
+          ),
+
+          ElevatedButton.icon(
+            icon: const Icon(Icons.save),
+            label: const Text("Save"),
+            onPressed: () {
+              if (_key.currentState!.validate()){
+                _key.currentState!.save();
+
+                tables[tableNum][rowNum] = [(rowNum+1).toString(),
+                          productName,
+                          int.parse(quantity),
+                          int.parse(basePrice),
+                          0
+                ];
+          
+                setState(() {
+                  _productNameController.clear();
+                  _basePriceController.clear();
+                  _quantityController.clear();
+                });
+
+                Navigator.of(context).pop();
+              }
+            }
+          ),
+
+       ],
+      );
+    }).then((_) {
+      setState(() {});
+    });
+  }
+  }
